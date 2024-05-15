c373103a4ab29e9649f0e5f9e543eb78e350edb6
1BM21CS060 <mohammedawaiz.cs21@bmsce.ac.in>
2024-05-15 22:44:49 +0530

    first commit

---
+++ src/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './HomePage.css';
+
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Avani </h1>
+      <h2>Avani </h2>
+      <h3>Avani </h3>
+      <h4>Avani </h4>
+      <h5>Avani </h5>
+      <h6>Avani </h6>
+      
+    </div>
+  );
+}
+
+export default HomePage;
